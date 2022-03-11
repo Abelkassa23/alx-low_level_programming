b0ea92a7cb8079c520a846fa1cd2c4d406200191
Abelkassa23 <kassabel23@gmail.com>
2022-03-11 20:19:54 +0000

    my first commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,6 +8,7 @@
 int main(void)
 {
 int c, i;
+
 for (c = '0'; c <= '9'; c++)
 {
 for (i = '0'; i <= '9'; i++)
@@ -16,10 +17,17 @@ if (c < i)
 {
 putchar(c);
 putchar(i);
+
 if (c != '8' || (c == '8' && i != '9'))
 {
 putchar(',');
 putchar(' ');
+}
+}
+}
+}
+
 putchar('\n');
+
 return (0);
 }
