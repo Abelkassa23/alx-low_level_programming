dd8d3177582bd23f333755a536d9c06a6f0705f1
Abelkassa23 <kassabel23@gmail.com>
2022-04-23 11:04:56 +0000

    free

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 
-/**
- *  * free_list - frees a list_t list.
- *   * @head: pointer to struct.
- *    */
+/*
+ * free_list - frees a list_t list.
+ * @head: pointer to struct.
+ */
 void free_list(list_t *head)
 {
 	list_t *tmp;
